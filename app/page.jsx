84302b7b4c0a1fc682a8aef08afa8d28d3a2ac8c
nevil-kristi-us <nevil.logicwind@gmail.com>
2024-02-14 11:39:37 +0530

    page changes

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <section className="py-24">
       <div className="container">
-        <h1 className="mt-2 text-3xl font-bold">Next js 13</h1>
+        <h1 className="mt-2 text-3xl font-bold">Next js 13 test</h1>
       </div>
     </section>
   );
